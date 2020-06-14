48f007dd63b0be084a422d5b828f9a96982e4c5e
Sean Weaver <weaversa@gmail.com>
2020-06-14 10:37:12 -0400

    polish

--- labs/saw/arithmetic-verification/arithmetic_implementations.c
+++ labs/saw/arithmetic-verification/arithmetic_implementations.c
@@ -6,7 +6,7 @@
 //
 // Add two 16-bit integers in a standard way, cast each to a local 32 bit
 // number and then add and return the result.
-uint32_t add_standard( uint16_t a, uint16_t b) {
+uint32_t add_standard(uint16_t a, uint16_t b) {
   uint32_t local_a = (uint32_t) a;
   uint32_t local_b = (uint32_t) b;
   uint32_t result;
@@ -31,7 +31,7 @@ uint32_t add_standard( uint16_t a, uint16_t b) {
 //                                       + [ b_1 ] [ b_0 ]
 //                                   =====================
 //                                 [ O_2 ] [ O_1 ] [ O_0 ]
-uint32_t add_textbook( uint16_t a, uint16_t b) {
+uint32_t add_textbook(uint16_t a, uint16_t b) {
   uint8_t a_1 = (0xff00 & a) >> 8;
   uint8_t a_0 = (0x00ff & a) >> 0;
   uint8_t b_1 = (0xff00 & b) >> 8;
@@ -56,7 +56,7 @@ uint32_t add_textbook( uint16_t a, uint16_t b) {
 //
 // Multiply two 32-bit integers in a standard way, cast each to a local 32 bit
 // number and then multiply and return the result.
-uint32_t multiply_standard( uint16_t a, uint16_t b) {
+uint32_t multiply_standard(uint16_t a, uint16_t b) {
   uint32_t local_a = (uint32_t) a;
   uint32_t local_b = (uint32_t) b;
   uint32_t result;
@@ -85,7 +85,7 @@ uint32_t multiply_standard( uint16_t a, uint16_t b) {
 // [    O_3    ] [    O_2    ] [    O_1    ] [    O_0    ]
 //
 // Where the outputs are the sums ofhe columns
-uint32_t multiply_textbook( uint16_t a, uint16_t b) {
+uint32_t multiply_textbook(uint16_t a, uint16_t b) {
   uint16_t a_1 = (0xff00 & a) >> 8;
   uint16_t a_0 = (0x00ff & a) >> 0;
   uint16_t b_1 = (0xff00 & b) >> 8;
@@ -118,58 +118,58 @@ uint32_t multiply_textbook( uint16_t a, uint16_t b) {
 //                                   
 //
 // Where the outputs are the sums ofhe columns
-uint32_t multiply_karatsuba( uint16_t a, uint16_t b) {
-    uint16_t a_1 = (0xff00 & a) >> 8;
-    uint16_t a_0 = (0x00ff & a) >>  0;
-    uint16_t b_1 = (0xff00 & b) >> 8;
-    uint16_t b_0 = (0x00ff & b) >>  0;
-    
-    uint32_t z0 = a_0 * b_0;
-    uint32_t z2 = a_1 * b_1;
-    uint32_t z1 = (a_1 + a_0) * (b_1 + b_0) - z0 - z2;
-    
-    uint32_t result = 0;
-    result += z0;
-    result += z1 << 8;
-    result += z2 << 16;
-    
-    return result;
+uint32_t multiply_karatsuba(uint16_t a, uint16_t b) {
+  uint16_t a_1 = (0xff00 & a) >> 8;
+  uint16_t a_0 = (0x00ff & a) >>  0;
+  uint16_t b_1 = (0xff00 & b) >> 8;
+  uint16_t b_0 = (0x00ff & b) >>  0;
+  
+  uint32_t z0 = a_0 * b_0;
+  uint32_t z2 = a_1 * b_1;
+  uint32_t z1 = (a_1 + a_0) * (b_1 + b_0) - z0 - z2;
+  
+  uint32_t result = 0;
+  result += z0;
+  result += z1 << 8;
+  result += z2 << 16;
+  
+  return result;
 }
 
 int main() {
-    #define NUM_TESTS 3
-    uint16_t tests[NUM_TESTS] = {0x0005, 0xbeef, 0xffff};
-    int i, j, num_tests = NUM_TESTS;
+  #define NUM_TESTS 3
+  uint16_t tests[NUM_TESTS] = {0x0005, 0xbeef, 0xffff};
+  int i, j, num_tests = NUM_TESTS;
 
-    printf("[INFO] ------------------\n"  );
-    printf("[INFO]   Addition Tests\n"   );
-    printf("[INFO] ------------------\n\n");
-    for(i = 0; i < num_tests; i++) {
-      for( j = 0; j < num_tests; j++) {
-        uint16_t a = tests[i];
-        uint16_t b = tests[j];
-        uint32_t r1 = add_standard(a, b);
-        uint32_t r2 = add_textbook(a, b);
-        printf("[INFO] add_standard (%08x, %08x) = %08x\n", a, b, r1);
-        printf("[INFO] add_textbook (%08x, %08x) = %08x\n", a, b, r2);
-        printf("\n");
-      }
-    }  
+  printf("[INFO] ------------------\n"  );
+  printf("[INFO]   Addition Tests\n"   );
+  printf("[INFO] ------------------\n\n");
+  for(i = 0; i < num_tests; i++) {
+    for(j = 0; j < num_tests; j++) {
+      uint16_t a = tests[i];
+      uint16_t b = tests[j];
+      uint32_t r1 = add_standard(a, b);
+      uint32_t r2 = add_textbook(a, b);
+      printf("[INFO] add_standard (%08x, %08x) = %08x\n", a, b, r1);
+      printf("[INFO] add_textbook (%08x, %08x) = %08x\n", a, b, r2);
+      printf("\n");
+    }
+  }  
 
-    printf("[INFO] ------------------------\n"  );
-    printf("[INFO]   Multiplication Tests\n"   );
-    printf("[INFO] ------------------------\n\n");
-    for(i = 0; i < num_tests; i++) {
-      for( j = 0; j < num_tests; j++) {
-        uint16_t a = tests[i];
-        uint16_t b = tests[j];
-        uint32_t r1 = multiply_standard(a, b);
-        uint32_t r2 = multiply_textbook(a, b);
-        uint32_t r3 = multiply_karatsuba(a, b);
-        printf("[INFO] multiply_standard (%08x, %08x) = %08x\n", a, b, r1);
-        printf("[INFO] multiply_textbook (%08x, %08x) = %08x\n", a, b, r2);
-        printf("[INFO] multiply_karatsuba(%08x, %08x) = %08x\n", a, b, r3);
-        printf("\n");
-      }
-    }    
+  printf("[INFO] ------------------------\n"  );
+  printf("[INFO]   Multiplication Tests\n"   );
+  printf("[INFO] ------------------------\n\n");
+  for(i = 0; i < num_tests; i++) {
+    for(j = 0; j < num_tests; j++) {
+      uint16_t a = tests[i];
+      uint16_t b = tests[j];
+      uint32_t r1 = multiply_standard(a, b);
+      uint32_t r2 = multiply_textbook(a, b);
+      uint32_t r3 = multiply_karatsuba(a, b);
+      printf("[INFO] multiply_standard (%08x, %08x) = %08x\n", a, b, r1);
+      printf("[INFO] multiply_textbook (%08x, %08x) = %08x\n", a, b, r2);
+      printf("[INFO] multiply_karatsuba(%08x, %08x) = %08x\n", a, b, r3);
+      printf("\n");
+    }
+  }    
 }
