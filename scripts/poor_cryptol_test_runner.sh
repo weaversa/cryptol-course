76b8b059703d8c9decc39ae3086904db39a8d250
WeeknightMVP <benk7405@hotmail.com>
2020-08-04 00:02:38 -0400

    Added conditionals to script; first try at failure detection

--- scripts/poor_cryptol_test_runner.sh
+++ scripts/poor_cryptol_test_runner.sh
@@ -5,24 +5,44 @@
 function extract_test_diff {
     for md in $( ls $1/*.md )
     do
-        basename=`basename $md .md`
+        dirbasename=$1/`basename $md .md`
+        tmp=$dirbasename.tmp
 
-        echo "Processing Markdown file $md ..."
+        echo "$0: Processing Markdown file $md ..."
 
-        icry=$1/$basename.icry
-        expected=$1/$basename.expected
-        actual=$1/$basename.actual
-        delta=$1/$basename.diff
+        echo "$0:   Replacing Windows newlines in $md with Linux newlines..."
+        dos2unix -n $md $tmp
+        mv $tmp $md
 
-        echo "  Extracting \`\`\`icry fences from $md; exporting commands to $icry and output to $expected ..."
-        sed -n '/^```icry/,/^```/ p' < $md | sed '/^```/ d' | grep -E "^[A-Za-z0-9:_']+?>" | sed -r "s/^[A-Za-z0-9:_']+?> ?(.*)$/\1/" > $icry
-        sed -n '/^```icry/,/^```/ p' < $md | sed '/^```/ d' | sed -r "s/^[A-Za-z0-9:_']+?> ?(.*)$//" | sed "/^$/d" > $expected
+        icry=$dirbasename.icry
+        expected=$dirbasename.expected
+        actual=$dirbasename.actual
+        delta=$dirbasename.diff
 
-        echo "  Logging interactive Cryptol running $icry to $actual ..."
-        cryptol -b $icry > $actual
+        if grep -q '^```icry' $md; then
+            echo "$0:   Extracting \`\`\`icry fences from $md; exporting commands to $icry and output to $expected ..."
+            sed -n '/^```icry/,/^```/ p' < $md | sed '/^```/ d' | grep -E "^[A-Za-z0-9:_']+?>" | sed -r "s/^[A-Za-z0-9:_']+?> ?(.*)$/\1/" > $icry
+            sed -n '/^```icry/,/^```/ p' < $md | sed '/^```/ d' | sed -r "s/^[A-Za-z0-9:_']+?> ?(.*)$//" | sed "/^$/d" > $expected
 
-        echo "  Logging difference between $expected and $actual to $delta ..."
-        diff $expected $actual > $delta
+            echo "$0:   Logging interactive Cryptol running $icry to $actual ..."
+            cryptol -b $icry > $actual
+
+            echo "$0:   Replacing Windows newlines in $expected and $actual with Linux newlines..."
+            dos2unix -n $expected $tmp && mv $tmp $expected
+            dos2unix -n $actual $tmp && mv $tmp $actual
+
+            echo "$0:   Logging difference between $expected and $actual to $delta ..."
+            diff $expected $actual > $delta
+
+            if grep -qE "Q.E.D.|Unsatisfiable" $delta; then
+                echo "Found Q.E.D. or Unsatisfiable; exiting..."
+                exit 1
+            fi
+        else
+            echo "$0:   $md did not have any \`\`\`icry fences; skipping..."
+        fi
+
+        # rm $icry $expected $actual $delta
     done
 }
 
