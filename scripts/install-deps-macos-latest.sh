0bdbe50ac599efae31433282586d8669d001e324
Matt Benke <mwbenke@radium.ncsc.mil>
2021-01-22 20:52:39 -0500

    Build -> Test/Deploy?

---
+++ scripts/install-deps-macos-latest.sh
@@ -0,0 +1,12 @@
+wget https://github.com/GaloisInc/cryptol/releases/download/2.10.0/cryptol-2.10.0-macOS-x86_64.tar.gz
+tar -xzf cryptol-2.10.0-macOS-x86_64.tar.gz
+export PATH=$PATH:$(pwd)/cryptol-2.10.0-macOS-x86_64/bin
+wget https://github.com/GaloisInc/saw-script/releases/download/v0.6/saw-0.6-macOS-x86_64.tar.gz
+tar -xzf saw-0.6-macOS-x86_64.tar.gz
+export PATH=$PATH:$(pwd)/saw-0.6-macOS-x86_64/bin
+cryptol --version
+saw --version
+curl -fsSL https://github.com/Z3Prover/z3/releases/download/z3-4.8.8/z3-4.8.8-x64-osx-10.14.6.zip -o z3-4.8.8-x64-osx-10.14.6.zip
+unzip -j z3-4.8.8-x64-osx-10.14.6.zip -d z3_downloaded
+export PATH=$PATH:$(pwd)/z3_downloaded
+z3 --version
