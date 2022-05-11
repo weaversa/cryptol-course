8dab574d91622f1cd9a112c0f0a35a38b7f794ca
Michaela Meier mfmeier <mfmeier@ip-10-113-85-211.evoforge.org>
2022-05-11 13:05:57 +0000

    test

--- .ci/ci_load.py
+++ .ci/ci_load.py
@@ -11,4 +11,6 @@ for (root, dirs, file) in os.walk(pwd):
     for f in file:
         if '.md' in f:
             if f not in not_cryptol:
-                c.load_file(os.path.join(root, f))
+                cry_file = os.path.join(root, f)
+                print("Loading file: " + cry_file)
+                c.load_file(cry_file)
