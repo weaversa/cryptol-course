1ea689c5d2ca8e649ec89f145c60c0933e6aa969
Michaela Meier mfmeier <mfmeier@ip-10-113-85-211.evoforge.org>
2022-05-16 13:50:16 +0000

    Adding prove

--- .ci/ci_helper.py
+++ .ci/ci_helper.py
@@ -1,4 +1,7 @@
 import cryptol
+from cryptol import solver
+from prover import *
+import random
 import sys
 
 def check(c, name, *args):
@@ -8,4 +11,28 @@ def check(c, name, *args):
         print(name + " failed on '" + expression + "' with arguments");
         for arg in args:
             print(arg)
+        sys.exit(1)
+
+def randBV(bits):
+    return cryptol.BV(bits, random.getrandbits())
+
+def randList(elements, bits):
+    array = []
+    element = 0
+    while element < elements:
+        array.append(randBV(bits))
+        element += 1
+    return array
+
+def safe(c, name):
+    result = c.safe(expr=name, solver=cryptol.solver.W4_YICES).result()
+    if result != True:
+        print(name + " is not safe")
+        sys.exit(1)
+
+def prove(c, name, strProver):
+    newProver = selectSolver(strProver)
+    result = c.prove(name, solver=newProver, timeout=120.0).result()
+    if result != True:
+        print(name + " failed to prove: " + result)
         sys.exit(1)
