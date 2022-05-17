9d6ba97858c3ee7d0189f8bd4f5c1320face5503
Michaela Meier mfmeier <mfmeier@ip-10-113-85-211.evoforge.org>
2022-05-17 14:03:12 +0000

    added CI to Transposition

---
+++ labs/Transposition/.ci/ci.py
@@ -0,0 +1,22 @@
+from ci_helper import *
+import cryptol
+
+c = cryptol.connect(reset_server=True)
+
+c.load_module("labs::Transposition::EsreverAnswers")
+
+print("Running tests in labs::Transposition::EsreverAnswers")
+
+check(c, "pi_test")
+
+c.load_module("labs::Transposition::RailFenceAnswers")
+
+print("Running tests in labs::Transposition::RailFenceAnswers")
+
+check(c, "pi_test")
+
+c.load_module("labs::Transposition::ScytaleAnswers")
+
+print("Running tests in labs::Transposition::ScytaleAnswers")
+
+check(c, "pi_test")
