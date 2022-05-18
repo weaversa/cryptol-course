0472340cffcb33ceace8e1eaccb5e5820bd5b42e
Michaela Meier mfmeier <mfmeier@ip-10-113-85-211.evoforge.org>
2022-05-18 13:50:27 +0000

    Adding xxhash32-ref.py

--- labs/Demos/SAW/xxHash/xxhash32-ref.py
+++ labs/Demos/SAW/xxHash/xxhash32-ref.py
@@ -14,64 +14,64 @@ def ptr_to_fresh(c : Contract, ty : LLVMType, name : Optional[str] = None) -> Tu
     ptr = c.alloc(ty, points_to = var)
     return (var, ptr)
 
-class Contract_XXH_rotl64(Contract):
+class Contract_XXH_rotl32(Contract):
     def specification(self) -> None:
-        value = self.fresh_var(i64, "value")
+        value = self.fresh_var(i32, "value")
         amt = self.fresh_var(i32, "amt")
 
         self.precondition_f("{amt} > 0")
-        self.precondition_f("{amt} < 64")
+        self.precondition_f("{amt} < 32")
         
         self.execute_func(value, amt)
 
         self.returns_f("{value} <<< {amt}")
 
 
-class Contract_XXH64_round(Contract):
+class Contract_XXH32_round(Contract):
     def specification(self) -> None:
-        acc = self.fresh_var(i64, "acc")
-        input_ = self.fresh_var(i64, "input")
+        acc = self.fresh_var(i32, "acc")
+        input_ = self.fresh_var(i32, "input")
 
         self.execute_func(acc, input_)
 
-        self.returns_f("XXH64_round {acc} {input_}")
+        self.returns_f("XXH32_round {acc} {input_}")
 
       
-class Contract_XXH64_avalanche(Contract):
+class Contract_XXH32_avalanche(Contract):
     def specification(self) -> None:
-        hash_ = self.fresh_var(i64, "hash")
+        hash_ = self.fresh_var(i32, "hash")
 
         self.execute_func(hash_)
 
         self.returns_f("XXH64_avalanche {hash_}")
 
 
-class Contract_XXH64_NULL (Contract):
+class Contract_XXH32_NULL (Contract):
     def specification(self) -> None:
-        length = self.fresh_var(i64, "length")
-        seed = self.fresh_var(i64, "seed")
+        length = self.fresh_var(i32, "length")
+        seed = self.fresh_var(i32, "seed")
 
         self.execute_func(null(), length, seed)
 
-        self.returns_f("XXH64`{{L=0}} zero {seed}")
+        self.returns_f("XXH32`{{L=0}} zero {seed}")
 
 
-class Contract_XXH64_top(Contract):
+class Contract_XXH32_top(Contract):
     def __init__(self, size):
         super().__init__()
         self.size = size
         
     def specification(self) -> None:
-        (input, input_p) = ptr_to_fresh(self, LLVMArrayType(i8, self.size)) 
-        length = cry_f("{self.size} : [64]")
-        seed = self.fresh_var(i64, "seed")
+        (input_, input_p) = ptr_to_fresh(self, LLVMArrayType(i8, self.size)) 
+        length = cry_f("{self.size} : [32]")
+        seed = self.fresh_var(i32, "seed")
 
         self.execute_func(input_p, length, seed)
 
-        self.returns_f("XXH64`{{L={self.size}}} {input} {seed}")
+        self.returns_f("XXH32`{{L={self.size}}} {input_} {seed}")
 
     
-class xxhash64EasyTest(unittest.TestCase):
+class xxhash32EasyTest(unittest.TestCase):
     def test_xxhash64(self):
         connect(reset_server=True)
         if __name__ == "__main__": view(LogResults())
@@ -82,7 +82,7 @@ class xxhash64EasyTest(unittest.TestCase):
         pwd = os.getcwd()
         
         cryname = pwd + "/labs/Demos/SAW/xxHash/xxhash.cry"
-        bcname = pwd + "/labs/Demos/SAW/xxHash/xxhash64-ref.bc"
+        bcname = pwd + "/labs/Demos/SAW/xxHash/xxhash32-ref.bc"
 
 
         print( "******" + cryname + "******")
@@ -91,24 +91,24 @@ class xxhash64EasyTest(unittest.TestCase):
 
         mod = llvm_load_module(bcname)
 
-        XXH_rotl64_result = llvm_verify(mod, "XXH_rotl64", Contract_XXH_rotl64())
-        self.assertIs(XXH_rotl64_result.is_success(), True)
+        XXH_rotl32_result = llvm_verify(mod, "XXH_rotl32", Contract_XXH_rotl32())
+        self.assertIs(XXH_rotl32_result.is_success(), True)
 
-        XXH64_round_result = llvm_verify(mod, "XXH64_round", Contract_XXH64_round())
-        self.assertIs(XXH64_round_result.is_success(), True)        
+        XXH32_round_result = llvm_verify(mod, "XXH32_round", Contract_XXH32_round())
+        self.assertIs(XXH32_round_result.is_success(), True)        
 
-        XXH64_avalanche_result = llvm_verify(mod, "XXH64_avalanche", Contract_XXH64_avalanche())
-        self.assertIs(XXH64_avalanche_result.is_success(), True)
+        XXH32_avalanche_result = llvm_verify(mod, "XXH32_avalanche", Contract_XXH32_avalanche())
+        self.assertIs(XXH32_avalanche_result.is_success(), True)
 
-        XXH64_NULL_result = llvm_verify(mod, "XXH64", Contract_XXH64_NULL())
-        self.assertIs(XXH64_NULL_result.is_success(), True)
+        XXH32_NULL_result = llvm_verify(mod, "XXH32", Contract_XXH32_NULL())
+        self.assertIs(XXH32_NULL_result.is_success(), True)
 
-        def XXH64_verify_print(size):
-            XXH64_result = llvm_verify(mod, "XXH64", Contract_XXH64_top(size), lemmas=[XXH_rotl64_result, XXH64_round_result, XXH64_avalanche_result, XXH64_NULL_result])
-            self.assertIs(XXH64_result.is_success(), True)
+        def XXH32_verify_print(size):
+            XXH32_result = llvm_verify(mod, "XXH32", Contract_XXH32_top(size), lemmas=[XXH_rotl32_result, XXH32_round_result, XXH32_avalanche_result, XXH32_NULL_result])
+            self.assertIs(XXH32_result.is_success(), True)
         
         for i in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 32, 64, 128]:
-            XXH64_verify_print(i)
+            XXH32_verify_print(i)
 
 if __name__ == "__main__":
     unittest.main()
