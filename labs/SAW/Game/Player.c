8bc7895a79419d2d3a2d1b5f70ff66361a9d6db6
djmorel <35698075+djmorel@users.noreply.github.com>
2022-05-27 07:08:44 -0400

    Added new directory to hold struct example
    Included the C source code in the initial commit.

---
+++ labs/SAW/Game/Player.c
@@ -0,0 +1,27 @@
+#include "Player.h"
+
+uint32_t initializeDefaultPlayer(player_t* player)
+{
+  // Variables
+  uint8_t i = 0;
+  uint32_t hp_default  = 10;
+  uint32_t atk_default = 5;
+  uint32_t def_default = 4;
+  uint32_t spd_default = 3;
+
+  // Initialize player according to some defaults
+  for (i = 0; i < MAX_NAME_LENGTH; i++)
+  {
+    // For simplicity, let's use A very cool name
+    // Assume ignoring the null terminator is fine too
+    // Remember, this is a dummy example ;)
+    player->name[i] = 'A';
+  }
+  player->level = 1;
+  player->hp  = hp_default;
+  player->atk = atk_default;
+  player->def = def_default;
+  player->spd = spd_default;
+
+  return SUCCESS;
+}
