0f907bbf12595551f4bf389159a7e643ef86b3cd
djmorel <djzmorel@gmail.com>
2022-05-27 18:49:40 +0000

    Modified SAW Game example file layout

---
+++ labs/SAW/Game/proof/Player.py
@@ -0,0 +1,9 @@
+# Imports
+import unittest
+from saw_client             import *
+from saw_client.crucible    import * 
+from saw_client.llvm        import * 
+from saw_client.proofscript import *
+from saw_client.llvm_type   import * 
+
+# TODO: Write the SAW contract and verify!
