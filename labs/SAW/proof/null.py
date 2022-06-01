e14a7adcebfd902c33ae17f78d909ddb0692fcc6
Taylor Ball Ball <trball@evoforge.org>
2022-06-01 19:29:10 +0000

    added files used in examples

---
+++ labs/SAW/proof/null.py
@@ -0,0 +1,26 @@
+import unittest
+from saw_client          import *
+from saw_client.crucible import *
+from saw_client.llvm     import *
+
+
+class isNull_Contract(Contract):
+    def specification(self):
+        # p = self.alloc(i32)
+
+        self.execute_func(null())
+
+        self.returns(cry("1 : [32]"))
+
+class LLVMAssertNullTest(unittest.TestCase):
+    def test_llvm_assert_null(self):
+        connect(reset_server=True)
+        if __name__ == "__main__": view(LogResults(verbose_failure=True))
+        bcname = '../src/null.bc'
+        mod    = llvm_load_module(bcname)
+
+        result = llvm_verify(mod, 'isNull', isNull_Contract())
+        self.assertIs(result.is_success(), True)
+
+if __name__ == "__main__":
+    unittest.main()
