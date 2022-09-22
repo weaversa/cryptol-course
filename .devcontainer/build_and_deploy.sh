44cf94e4bd73e8343550bd30963119b96ee0b06f
Sean Weaver <saweave@evoforge.org>
2022-09-22 09:46:17 -0400

    Updating containers

--- .devcontainer/build_and_deploy.sh
+++ .devcontainer/build_and_deploy.sh
@@ -1,2 +1,5 @@
 docker build - -t ghcr.io/weaversa/cryptol-course:2.13 < Dockerfile
 docker push ghcr.io/weaversa/cryptol-course:2.13
+
+docker build - -t ghcr.io/weaversa/cryptol-course:gitpod < Dockerfile.gitpod
+docker push ghcr.io/weaversa/cryptol-course:gitpod
