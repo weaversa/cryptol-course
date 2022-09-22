d67bdd590c556bee6bffb42c9099d2be93345b80
Sean Weaver <weaversa@gmail.com>
2022-09-22 09:51:08 -0400

    Separating containers

--- .devcontainer/build_and_deploy.sh
+++ .devcontainer/build_and_deploy.sh
@@ -1,4 +1,4 @@
-docker build - -t ghcr.io/weaversa/cryptol-course:2.13 < Dockerfile
+docker build - -t ghcr.io/weaversa/cryptol-course:2.13 < Dockerfile.2.13
 docker push ghcr.io/weaversa/cryptol-course:2.13
 
 docker build - -t ghcr.io/weaversa/cryptol-course:gitpod < Dockerfile.gitpod
