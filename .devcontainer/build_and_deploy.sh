c0bb1ec1d29b500505c5869cce294d1fed7bb423
Sean Weaver <weaversa@gmail.com>
2022-09-22 10:47:08 -0400

    Removing gitpod specific container

--- .devcontainer/build_and_deploy.sh
+++ .devcontainer/build_and_deploy.sh
@@ -1,2 +1,2 @@
-docker build - -t ghcr.io/weaversa/cryptol-course:2.13 < Dockerfile.2.13
+docker build - -t ghcr.io/weaversa/cryptol-course:2.13 < Dockerfile
 docker push ghcr.io/weaversa/cryptol-course:2.13
